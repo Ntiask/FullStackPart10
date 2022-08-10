2c596a431b4146d98cd2006d7651696dc1bda7a1
Niko <niko.tiaskorpi@gmail.com>
2022-08-10 13:56:43 +0300

    added conditional fonts

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+import {Platform} from 'react-native'
+
 const theme = {
     colors: {
       textPrimary: '#24292e',
@@ -12,7 +14,7 @@ const theme = {
       subheading: 16,
     },
     fonts: {
-      main: 'System',
+      main: Platform.OS === 'android' ? 'Sans-serif' : Platform.OS === 'ios' ? 'Arial': 'System'
     },
     fontWeights: {
       normal: '400',
